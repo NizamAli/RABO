8ca9571d0b0f9a0142b8a7f93ad9cc1bd886f10c
Nizam Ali <alinizam.shaik@gmail.com>
2020-02-29 15:22:32 +0530

    Added test case for Duplicate reference

--- src/test/java/com/cts/service/CustomerStatementServiceImplTest.java
+++ src/test/java/com/cts/service/CustomerStatementServiceImplTest.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,8 +15,6 @@ import org.mockito.junit.MockitoJUnitRunner;
 import com.cts.dto.RaboResponse;
 import com.cts.model.CustomerStatement;
 import com.cts.util.CustomerServiceEnum;
-
-import lombok.Builder;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CustomerStatementServiceImplTest {
@@ -29,7 +26,7 @@ public class CustomerStatementServiceImplTest {
 	}
 	
 	@Test
-	public void testProcessTransactions() { 
+	public void testSuccessProcessTransactions() { 
 		List<CustomerStatement> transactions = Arrays.asList(CustomerStatement.builder()
 																			.reference(111l)
 																			.accountNumber("1212")
@@ -40,5 +37,34 @@ public class CustomerStatementServiceImplTest {
 																			.build());
 		RaboResponse response = customerService.processTransactions(transactions);
 		assertEquals(CustomerServiceEnum.SUCCESSFUL.toString() ,response.getResult());
+	}
+	
+	@Test
+	public void testDuplicateReferenceTransactions() { 
+		
+		CustomerStatement statement1 = CustomerStatement.builder()
+		.reference(111l)
+		.accountNumber("1212")
+		.startBalance(10d)
+		.mutation(-5d)
+		.endBalance(5d)
+		.description("Duplicate Reference")
+		.build();
+		
+		CustomerStatement statement2 = CustomerStatement.builder()
+				.reference(111l)
+				.accountNumber("1213")
+				.startBalance(10d)
+				.mutation(-5d)
+				.endBalance(5d)
+				.description("Duplicate Reference")
+				.build();
+		
+		
+		List<CustomerStatement> transactions = new ArrayList<>();
+		transactions.add(statement1);
+		transactions.add(statement2);
+		RaboResponse response = customerService.processTransactions(transactions);
+		assertEquals(CustomerServiceEnum.DUPLICATE_REFERENCE.toString() ,response.getResult());
 	}
 }
